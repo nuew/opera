6aefcd4c40b553cc966fd1ba699768350f1ddcf5
nuew <code@nuew.net>
2019-05-10 14:27:33 -0400

    Add MalformedPacketError to crate error type

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use crate::packet::MalformedPacketError;
 use std::{
     error,
     fmt::{self, Display, Formatter},
@@ -5,16 +6,20 @@ use std::{
 };
 
 #[derive(Debug)]
+#[allow(variant_size_differences)]
 /// An error that has occured during decoding.
 pub enum Error {
     /// An error in an underlying I/O operation.
     Io(io::Error),
+    /// A received packet was malformed.
+    MalformedPacket(MalformedPacketError),
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             Error::Io(err) => err.fmt(f),
+            Error::MalformedPacket(err) => err.fmt(f),
         }
     }
 }
@@ -27,6 +32,7 @@ impl error::Error for Error {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
             Error::Io(err) => Some(err),
+            Error::MalformedPacket(err) => Some(err),
         }
     }
 }
@@ -34,6 +40,12 @@ impl error::Error for Error {
 impl From<io::Error> for Error {
     fn from(from: io::Error) -> Error {
         Error::Io(from)
+    }
+}
+
+impl From<MalformedPacketError> for Error {
+    fn from(from: MalformedPacketError) -> Error {
+        Error::MalformedPacket(from)
     }
 }
 
